fcdd0f108fccae301e9812ab34c492ebb31006ee
Siddharth Patel <sidharthpatel012@gmail.com>
2024-10-28 23:46:33 -0400

    add: attach a cupcake logo on the navbar

--- src/components/Navbar/navbar.tsx
+++ src/components/Navbar/navbar.tsx
@@ -1,30 +1,46 @@
-// src/components/Navbar.tsx
 import React from "react";
 import { Link } from "react-router-dom";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "./navbar.css";
+import cupcakeLogo from "../../assets/cupcake-logo.jpg";
 
 const Navbar: React.FC = () => {
 	return (
-		<nav>
-			<ul>
-				<li>
-					<Link to="/">Home</Link>
-				</li>
-				<li>
-					<Link to="/about">About</Link>
-				</li>
-				<li>
-					<Link to="/cupcakes">Cupcakes</Link>
-				</li>
-				<li>
-					<Link to="/new-arrivals">New Arrivals</Link>
-				</li>
-				<li>
-					<Link to="/contact">Contact</Link>
-				</li>
-			</ul>
-		</nav>
+		<header className="header">
+			<a>
+				<img src={cupcakeLogo} />
+			</a>
+			<nav className="navbar">
+				<ul>
+					<li>
+						<Link to="/">Home</Link>
+					</li>
+					<li>
+						<Link to="/cupcakes">Cupcakes</Link>
+					</li>
+					<li>
+						<Link to="/new-arrivals">New Arrivals</Link>
+					</li>
+					<li>
+						<Link to="/about">About</Link>
+					</li>
+					<li>
+						<Link to="/contact">Contact</Link>
+					</li>
+				</ul>
+			</nav>
+			<div className="icons">
+				<div className="search-icon">
+					<i className="fas fa-search"></i>
+				</div>
+				<div className="cart-icon">
+					<i className="fas fa-cart-arrow-down"></i>
+				</div>
+				<div className="menu-icon">
+					<i className="fas fa-bars"></i>
+				</div>
+			</div>
+		</header>
 	);
 };
 
